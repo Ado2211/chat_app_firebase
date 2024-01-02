2363631d723454e231ae1fd68575d1d069025b81
Ado2211 <mahmica@gmail.com>
2024-01-02 16:05:26 +0100

    chat view

--- lib/app/modules/chat/chat_controller.dart
+++ lib/app/modules/chat/chat_controller.dart
@@ -1,129 +1,53 @@
-import 'dart:async';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
-import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
-import 'package:intl/intl.dart';
 
 class ChatRoomController extends GetxController {
-  var isShowEmoji = false.obs;
-  int total_unread = 0;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  void sendMessage(String message, String friendUid) async {
+    try {
+      User? currentUser = _auth.currentUser;
 
-  late FocusNode focusNode;
-  late TextEditingController chatC;
-  late ScrollController scrollC;
+      if (currentUser != null) {
+        await _firestore
+            .collection('chats')
+            .doc(currentUser.uid)
+            .collection('messages')
+            .add({
+          'message': message,
+          'senderUid': currentUser.uid,
+          'timestamp': DateTime.now(),
+        });
 
-  Stream<QuerySnapshot<Map<String, dynamic>>> streamChats(String chat_id) {
-    CollectionReference chats = firestore.collection("chats");
-
-    return chats.doc(chat_id).collection("chat").orderBy("time").snapshots();
-  }
-
-  Stream<DocumentSnapshot<Object?>> streamFriendData(String friendEmail) {
-    CollectionReference users = firestore.collection("users");
-
-    return users.doc(friendEmail).snapshots();
-  }
-
-  void addEmojiToChat(Emoji emoji) {
-    chatC.text = chatC.text + emoji.emoji;
-  }
-
-  void deleteEmoji() {
-    chatC.text = chatC.text.substring(0, chatC.text.length - 2);
-  }
-
-  void newChat(String email, Map<String, dynamic> argument, String chat) async {
-    if (chat != "") {
-      CollectionReference chats = firestore.collection("chats");
-      CollectionReference users = firestore.collection("users");
-
-      String date = DateTime.now().toIso8601String();
-
-      await chats.doc(argument["chat_id"]).collection("chat").add({
-        "posiljaoc": email,
-        "primatelj": argument["friendEmail"],
-        "poruka": chat,
-        "time": date,
-        "isRead": false,
-        "groupTime": DateFormat.yMMMMd('en_US').format(DateTime.parse(date)),
-      });
-
-      Timer(
-        Duration.zero,
-        () => scrollC.jumpTo(scrollC.position.maxScrollExtent),
-      );
-
-      chatC.clear();
-
-      await users
-          .doc(email)
-          .collection("chats")
-          .doc(argument["chat_id"])
-          .update({
-        "lastTime": date,
-      });
-
-      final checkChatsFriend = await users
-          .doc(argument["friendEmail"])
-          .collection("chats")
-          .doc(argument["chat_id"])
-          .get();
-
-      if (checkChatsFriend.exists) {
-        // exist on friend DB
-        // first check total unread
-        final checkTotalUnread = await chats
-            .doc(argument["chat_id"])
-            .collection("chat")
-            .where("isRead", isEqualTo: false)
-            .where("pengirim", isEqualTo: email)
-            .get();
-
-        // total unread for friend
-        total_unread = checkTotalUnread.docs.length;
-
-        await users
-            .doc(argument["friendEmail"])
-            .collection("chats")
-            .doc(argument["chat_id"])
-            .update({"lastTime": date, "total_unread": total_unread});
-      } else {
-        // not exist on friend DB
-        await users
-            .doc(argument["friendEmail"])
-            .collection("chats")
-            .doc(argument["chat_id"])
-            .set({
-          "connection": email,
-          "lastTime": date,
-          "total_unread": 1,
+        await _firestore
+            .collection('chats')
+            .doc(friendUid)
+            .collection('messages')
+            .add({
+          'message': message,
+          'senderUid': currentUser.uid,
+          'timestamp': DateTime.now(),
         });
       }
+    } catch (e) {
+      print('Greška prilikom slanja poruke: $e');
     }
   }
 
-  @override
-  void onInit() {
-    chatC = TextEditingController();
-    scrollC = ScrollController();
-    focusNode = FocusNode();
-    focusNode.addListener(() {
-      if (focusNode.hasFocus) {
-        isShowEmoji.value = false;
-      }
-    });
-    super.onInit();
-  }
+  Stream<QuerySnapshot<Map<String, dynamic>>> getMessages(String friendUid) {
+    User? currentUser = _auth.currentUser;
 
-  @override
-  void onClose() {
-    chatC.dispose();
-    scrollC.dispose();
-    focusNode.dispose();
-    super.onClose();
+    if (currentUser != null) {
+      return _firestore
+          .collection('chats')
+          .doc(currentUser.uid)
+          .collection('messages')
+          .orderBy('timestamp', descending: true)
+          .snapshots();
+    }
+
+    return Stream.empty();
   }
 }
