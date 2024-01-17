7608b389331a9798fb9f8ab9daa0963aa9424797
Ado2211 <mahmica@gmail.com>
2024-01-17 22:39:50 +0100

    app bar chat view

--- lib/app/modules/chat/chat_view.dart
+++ lib/app/modules/chat/chat_view.dart
@@ -1,8 +1,8 @@
 import 'package:chat_app_firebase/app/modules/chat/chat_controller.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatRoomView extends StatelessWidget {
   final ChatRoomController controller = Get.put(ChatRoomController());
@@ -10,16 +10,83 @@ class ChatRoomView extends StatelessWidget {
   final String chatId;
   final String friendEmail;
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  
 
-  ChatRoomView({Key? key, required this.chatId, required this.friendEmail}) : super(key: key);
+  ChatRoomView({Key? key, required this.chatId, required this.friendEmail})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     String? currentUser = _auth.currentUser.toString();
     return Scaffold(
       appBar: AppBar(
-        title: Text('Chat s prijateljem'),
+        elevation: 15,
+        shadowColor: Color.fromARGB(255, 52, 75, 226),
+        toolbarHeight: 130,
+        automaticallyImplyLeading: false,
+        shape: ContinuousRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.elliptical(130, 70),
+            bottomRight: Radius.elliptical(70, 130),
+          ),
+        ),
+        flexibleSpace: Container(
+          padding: EdgeInsets.fromLTRB(16, 40, 16, 26),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 68, 114, 240),
+                Color.fromARGB(255, 123, 146, 238),
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp,
+            ),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Container(
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.arrow_back_ios),
+                  onPressed: () {
+                    Get.back();
+                  },
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: Image.asset(
+                    'assets/img/profile3.png',
+                    width: 60,
+                    height: 60,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Column(
+                  children: [
+                    Text(
+                      'Username',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Color.fromARGB(255, 254, 254, 254),
+                      ),
+                    ),
+                    Text(
+                      'username',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Color.fromARGB(255, 254, 254, 254),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -28,7 +95,7 @@ class ChatRoomView extends StatelessWidget {
           children: [
             Expanded(
               child: StreamBuilder<QuerySnapshot>(
-                stream: controller.getMessages(chatId), // Use chatId for getting messages
+                stream: controller.getMessages(chatId),
                 builder: (context, snapshot) {
                   if (snapshot.connectionState == ConnectionState.waiting) {
                     return Center(child: CircularProgressIndicator());
@@ -43,7 +110,8 @@ class ChatRoomView extends StatelessWidget {
                     reverse: true,
                     itemCount: messagesList.length,
                     itemBuilder: (context, index) {
-                      var messageData = messagesList[index].data() as Map<String, dynamic>;
+                      var messageData =
+                          messagesList[index].data() as Map<String, dynamic>;
                       return ListTile(
                         title: Text(messageData['message'] ?? ''),
                       );
